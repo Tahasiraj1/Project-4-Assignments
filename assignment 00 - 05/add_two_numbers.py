eb54e7323b01c3c9f34e99e6b818d24000d46db4
Tahasiraj1 <tahasiraj242@gmail.com>
2025-03-19 07:25:06 +0500

    first commit

---
+++ assignment 00 - 05/add_two_numbers.py
@@ -0,0 +1,8 @@
+def main():
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    sum = num1 + num2
+    print("Sum of the two numbers is: ", sum)
+
+if __name__ == "__main__":
+    main()
