045b5918691ebf42d78adc6ec03ad7b86002451d
Tahasiraj1 <tahasiraj242@gmail.com>
2025-03-20 13:39:29 +0500

    Add Madlibs assignment for interactive story generation

---
+++ assignments 1 - 6/1 - Madlibs/main.py
@@ -0,0 +1,6 @@
+adj = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+verb2 = input("Enter another verb: ")
+
+madlib = f"Computer programming is so {adj}! I love {verb}ing {verb2}!"
+print(madlib)
